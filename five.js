e5ba51f88bb16260d06fde9c3dc2fb72b675818a
subbarao <subbaraosoftwaretesting@gmail.com>
2020-08-25 22:46:47 +0800

    Updated six

--- five.js
+++ five.js
@@ -32,3 +32,34 @@ Math.round(10.9)
 
 // If it is less than 4 and firve then it is going to lower side ele upper side
 
+3.141592653589793
+Math.floor(Math.PI)
+3
+//generation random number
+Math.floor(Math.random()*10000)
+7001
+Math.floor(Math.random()*1000000)
+346184
+
+
+//between the range
+Math.floor(Math.random()*(max-min))+min
+Math.floor(Math.random()*(20-1))+1
+
+Math.floor(Math.random()*(20-1))+1
+8
+  Math.floor(Math.random()*(20-1))+1
+7
+  Math.floor(Math.random()*(20-1))+1
+13
+
+Math.E
+2.718281828459045
+Math.sin(1)
+0.8414709848078965
+Math.cos(1)
+0.5403023058681398
+Math.log(0)
+-Infinity
+Math.log(1)
+0   
